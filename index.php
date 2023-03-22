4a13a6c162953e7612c7db5bd901edb41c5b897d
Alejandro Martinez Morales <alexito_mor@outlook.es>
2023-03-21 22:08:49 -0600

    Add landing page

--- index.php
+++ index.php
@@ -8,9 +8,14 @@
   <link rel="stylesheet" type="text/css" href="librerias/bootstrap/css/bootstrap.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
   <link rel="stylesheet" type="text/css" href="css/menu.css">
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+  <link href='https://unpkg.com/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+
 
   <!--For sliders-->
   <link rel="stylesheet" href="css/flexslider.css" type="text/css">
+
+  <link rel="stylesheet" type="text/css" href="informativa/estilos.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
   <script src="js/jquery.flexslider.js"></script>
   <script type="text/javascript" charset="utf-8">
@@ -58,6 +63,7 @@
               </a>
             </li>
           </ul>
+
         </div>
         <!--/.nav-collapse -->
       </div>
@@ -65,35 +71,142 @@
     </div>
 
 
-      <div class="flexslider">
-        <ul class="slides">
-          <li>
-            <img src="img/sliders/1.jpg" alt="">
-            <section class="flex-caption">
-              <p>LOREM IPSUM 1</p>
-            </section>
-          </li>
-          <li>
-            <img src="img/sliders/2.jpg" alt="">
-            <section class="flex-caption">
-              <p>LOREM IPSUM 2</p>
-            </section>
-          </li>
-          <li>
-            <img src="img/sliders/3.jpg" alt="">
-            <section class="flex-caption">
-              <p>LOREM IPSUM 3</p>
-            </section>
-          </li>
-        </ul>
+    <div class="flexslider">
+      <ul class="slides">
+        <li>
+          <img src="img/sliders/Lavanderia4.jpg" alt="">
+          <section class="flex-caption">
+            <p>GRUPO-TECNOLAVADO</p>
+          </section>
+        </li>
+        <li>
+          <img src="img/sliders/Lavanderia2.jpg" alt="">
+          <section class="flex-caption">
+            <p>GRUPO-TECNOLAVADO</p>
+          </section>
+        </li>
+        <li>
+          <img src="img/sliders/Lavanderia5.jpg" alt="">
+          <section class="flex-caption">
+            <p>GRUPO-TECNOLAVADO</p>
+          </section>
+        </li>
+      </ul>
+    </div>
+
+
+    <p></p>
+
+  </div>
+  </div>
+  </div>
+  
+  <main>
+    <section class="services contenedor" id="servicio">
+      <h2 class="subtitulo">PRUEBA NUESTROS SERVICIOS</h2>
+      <div class="contenedor-servicio">
+        <img src="img/imagen23.jpeg" alt="">
+        <div class="checklist-servicio">
+          <div class="service">
+            <h3 class="n-service"><span class="number">1</span>Lavandería</h3>
+            <p> Lavamos y cuidamos tu ropa como si fuera de nosotros, utilizando los mejores detergentes.</p>
+          </div>
+          <div class="service">
+            <h3 class="n-service"><span class="number">2</span>Tintorería</h3>
+            <p> Tenemos servicio de 24 horas para que tengas lista tu ropa, en tu casa.</p>
+          </div>
+          <div class="service">
+            <h3 class="n-service"><span class="number">3</span>Planchado</h3>
+            <p> Planchamos tu ropa y la dejamos casi lista en tu closet, a domicilio.</p>
+          </div>
+        </div>
       </div>
+    </section>
+    <section class="gallery" id="galeria">
+      <div class="contenedor">
+        <h2 class="subtitulo">Galeria</h2>
+        <div class="contenedor-galeria">
+          <img src="img/imagen16.jpeg" class="img-galeria" alt="Hola">
+          <img src="img/imagen17.jpeg" class="img-galeria" alt="">
+          <img src="img/imagen18.jpeg" class="img-galeria" alt="">
+          <img src="img/imagen16.jpeg" class="img-galeria" alt="">
+          <img src="img/imagen17.jpeg" class="img-galeria" alt="">
+          <img src="img/imagen18.jpeg" class="img-galeria" alt="">
+        </div>
+      </div>
+    </section>
+    <div class="imagen-light">
+      <img src="img/close.svg" alt="" class="close">
+      <img src="" alt="" class="agregar-imagen">
+    </div>
+    <section class="contenedor" id="expertos">
+      <h2 class="subtitulo">HACEMOS MÁS FÁCIL TU VIDA:</h2>
+      <h2 class="subtitulo2">LAVANDERÍA A DOMICILIO:</h2>
+      <section class="experts">
+        <div class="cont-expert">
+          <img src="informativa/img/Notifications_Outline.svg" alt="">
+          <h3 class="n-expert">LLÁMANOS O MÁNDANOS WHATSAPP</h3>
+        </div>
+        <div class="cont-expert">
+          <img src="informativa/img/Road trip_Outline.svg" alt="">
+          <h3 class="n-expert">VAMOS POR TU ROPA</h3>
+        </div>
+        <div class="cont-expert">
+          <img src="informativa/img/Order Confirmed_Outline.svg" alt="">
+          <h3 class="n-expert">TE LA REGRESAMOS LAVADA Y DOBLADA</h3>
+        </div>
+      </section>
+    </section>
+  </main>
+  <section class="header1" id="inicio1">
 
 
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsum suscipit assumenda sunt, doloremque esse aperiam rerum quisquam quo expedita exercitationem maiores minus deleniti eum possimus voluptatem ipsa. Voluptas, autem.</p>
+    <div class="contenedor head1">
+      <h1 class="titulo1">GRUPO - TECNOLAVADO</h1>
+      <p class="copy1">TU ROPA LIMPIA EL MISMO DÍA, SIN CARGOS EXTRA</p>
+    </div>
 
-  </div>
-  </div>
-  </div>
+  </section>
+  <section class="services contenedor1" id="servicio1">
+    <h2 class="subtitulo1">TENEMOS SERVICIO EN ACATLÁN: BARRIO DE SAN MIGUEL, CALLE DE LA LIBERTAD 203</h2>
+    <div class="contenedor-servicio1">
+      <div class="checklist-servicio1">
+        <div class="service1">
+          <h3 class="n-service1"><span class=""></span>SUCURSALES CENTRO, ACATLÁN PUEBLA</h3>
+          <p> Calle de la Libertad 203 , Barrio de san Miguel.</p>
+        </div>
+        <div class="service1">
+          <h3 class="n-service1"><span class=""></span>Horarios</h3>
+          <p> Lunes – Domingo: 8:00 AM a 7:00 PM.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d947.5265927945975!2d-98.0522926!3d18.205035!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cf4135eff7d029%3A0xb979ea9eda4c64e!2sgrupo%20tecnolavado!5e0!3m2!1ses!2smx!4v1679174743820!5m2!1ses!2smx" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+  
+  <footer id="footer">
+    <div class="contenedor footer-content">
+      <div class="contact-us">
+        <h2 class="brand">© Grupo-Tecnolavado</h2>
+        <p>Lavandería & Tintorería</p>
+      </div>
+      <div class="social-media">
+        <a href="./" class="social-media-icon">
+          <i class='bx bxl-facebook'></i>
+        </a>
+        <a href="./" class="social-media-icon">
+          <i class='bx bxl-twitter'></i>
+        </a>
+        <a href="./" class="social-media-icon">
+          <i class='bx bxl-instagram'></i>
+        </a>
+      </div>
+    </div>
+    <div class="line"></div>
+  </footer>
+  <script src="informativa/js/menu.js"></script>
+  <script src="informativa/js/lightbox.js"></script>
 
 
 </body>
