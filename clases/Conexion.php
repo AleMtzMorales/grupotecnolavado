32db45dcaabbac37ad3dfda8620a2002e3d9378c
Alejandro Martinez Morales <alexito_mor@outlook.es>
2023-03-22 00:53:22 -0600

    is set the production files settings

--- clases/Conexion.php
+++ clases/Conexion.php
@@ -1,10 +1,10 @@
 <?php 
 
 	class conectar{
-		private $servidor="localhost";
-		private $usuario="root";
-		private $password="";
-		private $bd="ventas";
+		private $servidor="212.1.210.115";
+		private $usuario="grupotec_Grupo-Tecnolavado";
+		private $password="ale_1998";
+		private $bd="grupotec_lavanderia";
 
 		public function conexion(){
 			$conexion=mysqli_connect($this->servidor,
--- clases/Constants.php
+++ clases/Constants.php
@@ -3,8 +3,8 @@
 class Constants
 {
 //¡¡¡RECORDAR CAMBIAR EL HOST AL DOMINIO DEL HSOTING O SERVIDOR!!!
-  const HOST = 'http://localhost/ventas';    // protocolo + host, example: https://milavanderia.com
-
+  const HOST = 'https://grupo-tecnolavado.com.mx';    // protocolo + host, example: https://milavanderia.com
+  //const HOST = 'http://localhost/ventas';
   public static function getHost()
   {
     return self::HOST;
