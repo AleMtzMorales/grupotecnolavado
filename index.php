d4ff046a494574d5b301e17449f33fb984f53cc6
Alejandro Martinez Morales <alexito_mor@outlook.es>
2023-04-12 17:27:36 -0600

    changes

--- index.php
+++ index.php
@@ -1,226 +1,85 @@
+<?php 
+	
+	require_once "clases/Conexion.php";
+	$obj= new conectar();
+	$conexion=$obj->conexion();
+
+	$sql="SELECT * from usuarios where email='admin'";
+	$result=mysqli_query($conexion,$sql);
+	$validar=0;
+	if(mysqli_num_rows($result) > 0){
+		$validar=1;
+	}
+ ?>
+
+
 <!DOCTYPE html>
 <html>
-
 <head>
-  <title>Lavanderia tecnolavado</title>
-  <meta charset="utf-8">
-  <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-  <link rel="stylesheet" type="text/css" href="librerias/bootstrap/css/bootstrap.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
-  <link rel="stylesheet" type="text/css" href="css/menu.css">
-  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
-  <link href='https://unpkg.com/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
-
-
-  <!--For sliders-->
-  <link rel="stylesheet" href="css/flexslider.css" type="text/css">
-
-  <link rel="stylesheet" type="text/css" href="informativa/estilos.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-  <script src="js/jquery.flexslider.js"></script>
-  <script type="text/javascript" charset="utf-8">
-    $(window).load(function() {
-      $('.flexslider').flexslider({
-        touch: true,
-        pauseOnAction: false,
-        pauseOnHover: false,
-      });
-    });
-  </script>
-
-
-
-
-
+	<title>Login de usuario</title>
+	<link rel="stylesheet" type="text/css" href="librerias/bootstrap/css/bootstrap.css">
+	<script src="librerias/jquery-3.2.1.min.js"></script>
+	<script src="js/funciones.js"></script>
+<link rel="icon" type="image/png" href="img/logoChico.png" />
 
 </head>
-
-<body>
-
-  <!-- Begin Navbar -->
-  <div id="nav">
-    <div class="navbar  navbar-default navbar-fixed-top " data-spy="affix" data-offset-top="100">
-      <div class="container ">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="" href=""><img class="img-responsive logo img-thumbnail align-top" src="img/imagen23.jpeg" alt="" width="120px" height="50px"></a>
-        </div>
-
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav navbar-right">
-            </li>
-            <li class="dropdown">
-            <li>
-              <a href="login.php">
-                <button class="btn btn-primary">
-                  Iniciar sesión <span class="glyphicon glyphicon-log-in"></span>
-                </button>
-              </a>
-            </li>
-          </ul>
-
-        </div>
-        <!--/.nav-collapse -->
-      </div>
-      <!--/.contatiner -->
-    </div>
-
-
-    <div class="flexslider">
-      <ul class="slides">
-        <li>
-          <img src="img/sliders/Lavanderia4.jpg" alt="">
-          <section class="flex-caption">
-            <p>GRUPO-TECNOLAVADO</p>
-          </section>
-        </li>
-        <li>
-          <img src="img/sliders/Lavanderia2.jpg" alt="">
-          <section class="flex-caption">
-            <p>GRUPO-TECNOLAVADO</p>
-          </section>
-        </li>
-        <li>
-          <img src="img/sliders/Lavanderia5.jpg" alt="">
-          <section class="flex-caption">
-            <p>GRUPO-TECNOLAVADO</p>
-          </section>
-        </li>
-      </ul>
-    </div>
-
-
-    <p></p>
-
-  </div>
-  </div>
-  </div>
-
-  <main>
-    <section class="services contenedor" id="servicio">
-      <h2 class="subtitulo">PRUEBA NUESTROS SERVICIOS</h2>
-      <div class="contenedor-servicio">
-        <img src="img/imagen23.jpeg" alt="">
-        <div class="checklist-servicio">
-          <div class="service">
-            <h3 class="n-service"><span class="number">1</span>Lavandería</h3>
-            <p> Lavamos y cuidamos tu ropa como si fuera de nosotros, utilizando los mejores detergentes.</p>
-          </div>
-          <div class="service">
-            <h3 class="n-service"><span class="number">2</span>Tintorería</h3>
-            <p> Tenemos servicio de 24 horas para que tengas lista tu ropa, en tu casa.</p>
-          </div>
-          <div class="service">
-            <h3 class="n-service"><span class="number">3</span>Planchado</h3>
-            <p> Planchamos tu ropa y la dejamos casi lista en tu closet, a domicilio.</p>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="gallery" id="galeria">
-      <div class="contenedor">
-        <h2 class="subtitulo">Galeria</h2>
-        <div class="contenedor-galeria">
-          <img src="img/imagen16.jpeg" class="img-galeria" alt="Hola">
-          <img src="img/imagen17.jpeg" class="img-galeria" alt="">
-          <img src="img/imagen18.jpeg" class="img-galeria" alt="">
-          <img src="img/imagen16.jpeg" class="img-galeria" alt="">
-          <img src="img/imagen17.jpeg" class="img-galeria" alt="">
-          <img src="img/imagen18.jpeg" class="img-galeria" alt="">
-        </div>
-      </div>
-    </section>
-    <div class="imagen-light">
-      <img src="img/close.svg" alt="" class="close">
-      <img src="" alt="" class="agregar-imagen">
-    </div>
-    <section class="contenedor" id="expertos">
-      <h2 class="subtitulo">HACEMOS MÁS FÁCIL TU VIDA:</h2>
-      <h2 class="subtitulo2">LAVANDERÍA A DOMICILIO:</h2>
-      <section class="experts">
-        <div class="cont-expert">
-          <img src="informativa/img/Notifications_Outline.svg" alt="">
-          <h3 class="n-expert">LLÁMANOS O MÁNDANOS WHATSAPP</h3>
-        </div>
-        <div class="cont-expert">
-          <img src="informativa/img/Road trip_Outline.svg" alt="">
-          <h3 class="n-expert">VAMOS POR TU ROPA</h3>
-        </div>
-        <div class="cont-expert">
-          <img src="informativa/img/Order Confirmed_Outline.svg" alt="">
-          <h3 class="n-expert">TE LA REGRESAMOS LAVADA Y DOBLADA</h3>
-        </div>
-      </section>
-    </section>
-  </main>
-  <section class="header1" id="inicio1">
-
-
-    <div class="contenedor head1">
-      <h1 class="titulo1">GRUPO - TECNOLAVADO</h1>
-      <p class="copy1">TU ROPA LIMPIA EL MISMO DÍA, SIN CARGOS EXTRA</p>
-    </div>
-
-  </section>
-  <section class="contenedor head1" style="text-align: center !important; padding-bottom: 8rem;" id="servicio1">
-    <h2 class="subtitulo1" style="padding-top: 6rem;">TENEMOS SERVICIO EN ACATLÁN: BARRIO DE SAN MIGUEL, CALLE DE LA LIBERTAD 203</h2>
-    <div class="contenedor-servicio1">
-      <div class="checklist-servicio1">
-        <div class="service1">
-          <h3 class="n-service1"><span class=""></span>SUCURSALES CENTRO, ACATLÁN PUEBLA</h3>
-          <p style="color: black;"> Calle de la Libertad 203 , Barrio de san Miguel.</p>
-        </div>
-        <div class="service2">
-          <h3 class="n-service2"><span class=""></span>Horarios</h3>
-          <p style="color: black;"> Lunes – Domingo: 8:00 AM a 7:00 PM.</p>
-        </div>
-
-        <div class="service3">
-          <h3 class="n-service3"><span class=""></span>Contacto</h3>
-          <a href="tel:+529535416217" target="_blank">9535416217</a>
-          <div style="padding: 1rem;"></div>
-          <a class="btn btn-success btn-lg" href="https://api.whatsapp.com/send?phone=529535416217" target="_blank">
-            <i class='bx bx-whatsapp '></i>
-            Whatsapp</a>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <section class="services contenedor1"  >
-  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d947.5265927945975!2d-98.0522926!3d18.205035!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cf4135eff7d029%3A0xb979ea9eda4c64e!2sgrupo%20tecnolavado!5e0!3m2!1ses!2smx!4v1679174743820!5m2!1ses!2smx" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-  </section>
-
-
-  <footer id="footer">
-    <div class="contenedor footer-content">
-      <div class="contact-us">
-        <h2 class="brand">© Grupo-Tecnolavado</h2>
-        <p>Lavandería & Tintorería</p>
-      </div>
-      <div class="social-media">
-        <a href="./" class="social-media-icon">
-          <i class='bx bxl-facebook'></i>
-        </a>
-        <a href="./" class="social-media-icon">
-          <i class='bx bxl-twitter'></i>
-        </a>
-        <a href="./" class="social-media-icon">
-          <i class='bx bxl-instagram'></i>
-        </a>
-      </div>
-    </div>
-    <div class="line"></div>
-  </footer>
-  <script src="informativa/js/menu.js"></script>
-  <script src="informativa/js/lightbox.js"></script>
-
-
+<body style="background: url(img/imagen8.jpeg);width: 100%;height: 100px; background-repeat: no-repeat;">
+	<br><br><br>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4"></div>
+			<div class="col-sm-4">
+				<div class="panel panel-primary">
+					<div class="panel panel-heading">LAVANDERÍA Y TINTORERÍA</div>
+					<div class="panel panel-body">
+						<p>
+							<img src="img/imagen23.jpeg" class="img-responsive"  height="150">
+						</p>
+						<form id="frmLogin">
+							<label>Usuario</label>
+							<input type="text" class="form-control input-sm" name="usuario" id="usuario">
+							<label>Password</label>
+							<input type="password" name="password" id="password" class="form-control input-sm">
+							<p></p>
+							<span class="btn btn-primary btn-sm" id="entrarSistema">Entrar</span>
+							<?php  if(!$validar): ?>
+							<a href="registro.php" class="btn btn-danger btn-sm">Registrar</a>
+							<?php endif; ?>
+						</form>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-4"></div>
+		</div>
+	</div>
 </body>
-
 </html>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('#entrarSistema').click(function(){
+
+		vacios=validarFormVacio('frmLogin');
+
+			if(vacios > 0){
+				alert("Debes llenar todos los campos!!");
+				return false;
+			}
+
+		datos=$('#frmLogin').serialize();
+		$.ajax({
+			type:"POST",
+			data:datos,
+			url:"procesos/regLogin/login.php",
+			success:function(r){
+
+				if(r==1){
+					window.location="vistas/articulos.php";
+				}else{
+					alert("No se pudo acceder :(");
+				}
+			}
+		});
+	});
+	});
+</script>
