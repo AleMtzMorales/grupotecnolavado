043a3ac102456951d746e7a6ca8927a5d2478e6f
Alejandro Martinez Morales <alexito_mor@outlook.es>
2023-04-12 18:09:29 -0600

    update folders

--- app/clases/Constants.php
+++ app/clases/Constants.php
@@ -3,7 +3,7 @@
 class Constants
 {
 //¡¡¡RECORDAR CAMBIAR EL HOST AL DOMINIO DEL HSOTING O SERVIDOR!!!
-  const HOST = 'http://localhost/ventas';    // protocolo + host, example: https://milavanderia.com
+  const HOST = 'http://localhost/ventas/app';    // protocolo + host, example: https://milavanderia.com
 
   public static function getHost()
   {
