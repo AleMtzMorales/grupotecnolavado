4cdf2e12f26b5c4163890ef998c21d2beb9ce8de
Alejandro Martinez Morales <alexito_mor@outlook.es>
2023-04-12 22:29:36 -0600

    add viewport to fix responsive login view mobile and desktop

--- app/index.php
+++ app/index.php
@@ -1,28 +1,31 @@
-<?php 
-	
-	require_once "clases/Conexion.php";
-	$obj= new conectar();
-	$conexion=$obj->conexion();
+<?php
 
-	$sql="SELECT * from usuarios where email='admin'";
-	$result=mysqli_query($conexion,$sql);
-	$validar=0;
-	if(mysqli_num_rows($result) > 0){
-		$validar=1;
-	}
- ?>
+require_once "clases/Conexion.php";
+$obj = new conectar();
+$conexion = $obj->conexion();
+
+$sql = "SELECT * from usuarios where email='admin'";
+$result = mysqli_query($conexion, $sql);
+$validar = 0;
+if (mysqli_num_rows($result) > 0) {
+	$validar = 1;
+}
+?>
 
 
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>Login de usuario</title>
+	<meta name="viewport" content="width=device-width, initial-scale = 1">
 	<link rel="stylesheet" type="text/css" href="librerias/bootstrap/css/bootstrap.css">
 	<script src="librerias/jquery-3.2.1.min.js"></script>
 	<script src="js/funciones.js"></script>
-<link rel="icon" type="image/png" href="img/logoChico.png" />
+	<link rel="icon" type="image/png" href="img/logoChico.png" />
 
 </head>
+
 <body style="background: url(img/imagen8.jpeg);width: 100%;height: 100px; background-repeat: no-repeat;">
 	<br><br><br>
 	<div class="container">
@@ -33,7 +36,7 @@
 					<div class="panel panel-heading">LAVANDERÍA Y TINTORERÍA</div>
 					<div class="panel panel-body">
 						<p>
-							<img src="img/imagen23.jpeg" class="img-responsive"  height="150">
+							<img src="img/imagen23.jpeg" class="img-responsive" height="150">
 						</p>
 						<form id="frmLogin">
 							<label>Usuario</label>
@@ -42,8 +45,8 @@
 							<input type="password" name="password" id="password" class="form-control input-sm">
 							<p></p>
 							<span class="btn btn-primary btn-sm" id="entrarSistema">Entrar</span>
-							<?php  if(!$validar): ?>
-							<a href="registro.php" class="btn btn-danger btn-sm">Registrar</a>
+							<?php if (!$validar) : ?>
+								<a href="registro.php" class="btn btn-danger btn-sm">Registrar</a>
 							<?php endif; ?>
 						</form>
 					</div>
@@ -53,33 +56,34 @@
 		</div>
 	</div>
 </body>
+
 </html>
 
 <script type="text/javascript">
-	$(document).ready(function(){
-		$('#entrarSistema').click(function(){
+	$(document).ready(function() {
+		$('#entrarSistema').click(function() {
 
-		vacios=validarFormVacio('frmLogin');
+			vacios = validarFormVacio('frmLogin');
 
-			if(vacios > 0){
+			if (vacios > 0) {
 				alert("Debes llenar todos los campos!!");
 				return false;
 			}
 
-		datos=$('#frmLogin').serialize();
-		$.ajax({
-			type:"POST",
-			data:datos,
-			url:"procesos/regLogin/login.php",
-			success:function(r){
+			datos = $('#frmLogin').serialize();
+			$.ajax({
+				type: "POST",
+				data: datos,
+				url: "procesos/regLogin/login.php",
+				success: function(r) {
 
-				if(r==1){
-					window.location="vistas/articulos.php";
-				}else{
-					alert("No se pudo acceder :(");
+					if (r == 1) {
+						window.location = "vistas/articulos.php";
+					} else {
+						alert("No se pudo acceder :(");
+					}
 				}
-			}
+			});
 		});
-	});
 	});
 </script>
